cf410ab5155382683b3f7cb9539c645c31fd72b9
priyanshu agrawal <priyanshuagrawal303@gmail.com>
2026-04-15 23:50:33 +0530

    leraned deque

--- queue/questions.java
+++ queue/questions.java
@@ -1,5 +1,6 @@
 package queue;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -52,22 +53,30 @@ public class questions {
     public static void main(String[] args) {
         // String s = "aabccxb";
         // fristNonRepeatingInStreamOfChar(s);
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        q.add(4);
-        q.add(5);
-        q.add(6);
-        q.add(7);
-        q.add(8);
-        q.add(9);
-        q.add(10);
-        System.out.println(q);
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // q.add(6);
+        // q.add(7);
+        // q.add(8);
+        // q.add(9);
+        // q.add(10);
+        // System.out.println(q);
 
-        // interLeave(q);
-        reverseQueue(q);
-        System.out.println(q);
+        // // interLeave(q);
+        // reverseQueue(q);
+        // System.out.println(q);
+
+
+
+        //deque using jcf
+        Deque<Integer> d = new LinkedList<>();
+        d.addFirst(3); 
+        d.addFirst(4); 
+        System.out.println(d);
     }
 
 
